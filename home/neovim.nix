62e2736e193e5fee759d0e8f25ff30ae6dc832d7
Tomas Kala <me@tomaskala.com>
2025-06-09 17:01:12 +0200

    neovim: Enable pyright analysis

--- home/neovim.nix
+++ home/neovim.nix
@@ -85,12 +85,6 @@
               -- Using Ruff's import organizer.
               disableOrganizeImports = true,
             },
-            python = {
-              analysis = {
-                -- Ignore all files for analysis to exclusively use Ruff for linting.
-                ignore = { "*" },
-              },
-            },
           },
         }
       '';
