7f07b0a8b022a789b250f603d31f020301bea3e9
Tomas Kala <me@tomaskala.com>
2025-08-12 22:54:32 +0200

    Enable tandoor

--- modules/nixos-server/tandoor.nix
+++ modules/nixos-server/tandoor.nix
@@ -20,20 +20,33 @@ in
         address = "localhost";
 
         extraConfig = {
+          SECRET_KEY_FILE = ""; # TODO
+          ALLOWED_HOSTS = cfg.domain;
           DB_ENGINE = "django.db.backends.postgresql";
           POSTGRES_HOST = "/run/postgresql";
           POSTGRES_DB = "tandoor";
           POSTGRES_USER = "tandoor";
+          REMOTE_USER_AUTH = 1;
         };
       };
 
-      # TODO: Remote auth.
       caddy = {
         enable = true;
 
-        virtualHosts.${cfg.domain}.extraConfig = ''
-          reverse_proxy :${builtins.toString config.services.tandoor-recipes.port}
-        '';
+        virtualHosts.${cfg.domain}.extraConfig =
+          if config.infra.authentication.enable then
+            ''
+              forward_auth :${builtins.toString config.infra.authentication.port} {
+                uri /api/authz/forward-auth
+                copy_headers Remote-User
+              }
+
+              reverse_proxy :${builtins.toString config.services.tandoor-recipes.port}
+            ''
+          else
+            ''
+              reverse_proxy :${builtins.toString config.services.tandoor-recipes.port}
+            '';
       };
 
       postgresql = {
