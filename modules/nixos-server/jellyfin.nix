4c8ace52c8ee3fe14b3b18dddf95e08f84debb47
Tomas Kala <me@tomaskala.com>
2025-12-03 21:10:36 +0100

    Jellyfin: Get rid of SSO

--- modules/nixos-server/jellyfin.nix
+++ modules/nixos-server/jellyfin.nix
@@ -41,32 +41,6 @@ in
           '';
         };
       };
-
-      authelia.instances.main.settings.identity_providers.oidc.clients = [
-        {
-          client_name = "Jellyfin";
-          client_id = "ak1uYO_QiSPnWCDYE2.7ou8dJpuzMSn.zheGGtxHAO-h6d5GLYMRTNVTq~wefxTZ9bHKIBWJ";
-          client_secret = "$pbkdf2-sha512$310000$WVcn/7wPDz6GkNCIof9BVg$3Px.2hEpdEQWdE5cNyZyCw54JAZ9fvyh7b6KJJpqYUw4QsxsJ0lStggIQ7.DpSRy4NhN84NHRO0SjsiZsVJaGg";
-          public = false;
-          authorization_policy = "one_factor";
-          require_pkce = true;
-          pkce_challenge_method = "S256";
-          redirect_uris = [
-            "https://${domain}/sso/OID/redirect/authelia"
-            "http://${domain}/sso/OID/redirect/authelia"
-          ];
-          scopes = [
-            "openid"
-            "profile"
-            "groups"
-          ];
-          response_types = [ "code" ];
-          grant_types = [ "authorization_code" ];
-          access_token_signed_response_alg = "none";
-          userinfo_signed_response_alg = "none";
-          token_endpoint_auth_method = "client_secret_post";
-        }
-      ];
     };
 
     environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
