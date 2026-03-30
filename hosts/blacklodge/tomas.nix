f102c7d6799b12db302d9e7947b7989cac1ac124
Tomas Kala <me@tomaskala.com>
2026-03-30 19:05:06 +0200

    Get rid of starship

--- hosts/blacklodge/tomas.nix
+++ hosts/blacklodge/tomas.nix
@@ -8,7 +8,6 @@
     ../../modules/home-manager/neovim.nix
     ../../modules/home-manager/programs.nix
     ../../modules/home-manager/ssh.nix
-    ../../modules/home-manager/starship.nix
     ../../modules/home-manager/yt-dlp.nix
     ../../modules/home-manager/zellij.nix
   ];
--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -5,6 +5,19 @@
     enable = true;
     package = pkgs.unstable.fish;
 
+    functions = {
+      fish_prompt.body = ''
+        set -l last_status $status
+        set -l stat
+
+        if test $last_status -ne 0
+          set stat (set_color red)"[$last_status]"(set_color --reset)
+        end
+
+        string join ''' -- (set_color blue) (prompt_pwd --dir-length=0) (set_color --reset) (set_color green) (fish_git_prompt) (set_color --reset) $stat '> '
+      '';
+    };
+
     interactiveShellInit = # fish
       ''
         set -gx EMAIL me@tomaskala.com
