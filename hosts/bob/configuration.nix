d141c5fd0d1d832096fe46695259dd11ade091c3
Tomas Kala <me@tomaskala.com>
2026-04-26 15:34:40 +0200

    Cleanup keepalive service

--- hosts/bob/configuration.nix
+++ hosts/bob/configuration.nix
@@ -238,42 +238,48 @@ in
     };
 
     systemd = {
-      services.keepalive.serviceConfig = {
-        EnvironmentFile = config.age.secrets.healthchecks-env.path;
-        Type = "oneshot";
+      services.keepalive = {
+        description = "Keepalive signal";
 
-        DynamicUser = true;
-        NoNewPrivileges = true;
-        ProtectSystem = "strict";
-        PrivateTmp = true;
-        ProtectHome = true;
-        ProtectClock = true;
-        ProtectKernelLogs = true;
-        ProtectKernelModules = true;
-        ProtectControlGroups = true;
-        RestrictNamespaces = true;
-        RestrictSUIDGUID = true;
-        UMask= "0077";
-        LockPersonality = true;
-        RestrictRealtime = true;
-        MemoryDenyWriteExecute = true;
+        serviceConfig = {
+          EnvironmentFile = config.age.secrets.healthchecks-env.path;
+          Type = "oneshot";
 
-        ExecStart =
-          let
-            script = pkgs.writeShellApplication {
-              name = "keepalive";
-              runtimeInputs = [ pkgs.curl ];
+          DynamicUser = true;
+          NoNewPrivileges = true;
+          ProtectSystem = "strict";
+          PrivateTmp = true;
+          ProtectHome = true;
+          ProtectClock = true;
+          ProtectKernelLogs = true;
+          ProtectKernelModules = true;
+          ProtectControlGroups = true;
+          RestrictNamespaces = true;
+          RestrictSUIDGUID = true;
+          UMask= "0077";
+          LockPersonality = true;
+          RestrictRealtime = true;
+          MemoryDenyWriteExecute = true;
 
-              text = ''
-                curl --fail --silent --show-error --max-time 10 --retry 5 --output /dev/null "$HC_URL"
-              '';
-            };
-          in
-          lib.getExe script;
+          ExecStart =
+            let
+              script = pkgs.writeShellApplication {
+                name = "keepalive";
+                runtimeInputs = [ pkgs.curl ];
+
+                text = ''
+                  curl --fail --silent --show-error --max-time 10 --retry 5 --output /dev/null "$HC_URL"
+                '';
+              };
+            in
+            lib.getExe script;
+        };
       };
 
       timers.keepalive = {
+        description = "Keepalive timer";
         wantedBy = [ "timers.target" ];
+        partOf = [ "keepalive.service" ];
 
         timerConfig = {
           OnBootSec = "10m";
