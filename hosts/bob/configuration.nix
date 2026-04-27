ef41488c87530f4d18873ec6459b531b64210e86
Tomas Kala <me@tomaskala.com>
2026-04-27 23:15:02 +0200

    bob: Switch to ZFS

--- hosts/bob/configuration.nix
+++ hosts/bob/configuration.nix
@@ -13,6 +13,7 @@ let
 in
 {
   imports = [
+    ./disko-config.nix
     ./hardware-configuration.nix
     ../../modules/nixos-common/nix.nix
     ../../modules/nixos-server/audiobookshelf.nix
@@ -35,9 +36,19 @@ in
       options = "--delete-older-than 30d";
     };
 
-    boot.loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
+    boot = {
+      initrd.supportedFilesystems = [ "zfs" ];
+      supportedFilesystems = [ "zfs" ];
+
+      zfs = {
+        package = pkgs.zfs_2_4;
+        forceImportRoot = false;
+      };
+
+      loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+      };
     };
 
     system.stateVersion = "25.05";
@@ -220,6 +231,7 @@ in
 
     networking = {
       inherit hostName;
+      hostId = "527acb68";
 
       firewall = {
         enable = true;
--- hosts/bob/hardware-configuration.nix
+++ hosts/bob/hardware-configuration.nix
@@ -26,22 +26,6 @@
     };
   };
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/44a0b4fb-7c37-4fa8-ac0f-97adfb2ed77b";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/AF7C-2C7D";
-    fsType = "vfat";
-    options = [
-      "fmask=0077"
-      "dmask=0077"
-    ];
-  };
-
-  swapDevices = [ { device = "/dev/disk/by-uuid/e3ee895c-d74a-4699-9b93-b1364f0ea60e"; } ];
-
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
